3bb761b66de98f5bc5db40cc4756cf4e2e087957
Andreas Neumann <andneuma@posteo.de>
2025-12-02 16:20:18 +0100

    Migrate base styling to SVK setup

--- src/routes/[locale]/+layout.svelte
+++ src/routes/[locale]/+layout.svelte
@@ -1,11 +1,24 @@
 <script>
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 	import favicon from '$lib/assets/favicon.svg';
+	import '../../app.scss';
 
 	let { children } = $props();
+
+	onMount(async () => {
+		if (!browser) return;
+		await import('bootstrap');
+		await import('@fortawesome/fontawesome-free/js/all.js');
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="bg-light min-vh-100">
+	<div class="container py-5">
+		{@render children()}
+	</div>
+</div>

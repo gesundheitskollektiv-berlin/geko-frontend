0ff785f2a5d12891b9f1d3c374c38c8cbbc15322
Andreas Neumann <andneuma@posteo.de>
2025-12-02 22:13:07 +0100

    Fix contact block

--- src/lib/components/page-blocks/ContactBlock.svelte
+++ src/lib/components/page-blocks/ContactBlock.svelte
@@ -16,93 +16,135 @@
   const toggleMap = () => (showGoogleMap = !showGoogleMap);
 </script>
 
-<section id={sectionId} class={backgroundClass}>
-  <div class="container">
+<section id={sectionId} class={`${backgroundClass} py-0`}>
+  <div class="container" id="contact">
     <div class="row justify-content-center">
-      <div class="col-lg-7 col-md-10 my-5">
-          <h2>{data.title}</h2>
+      <div class="col-lg-7 col-md-10">
+        <h2 class="pt-3 pt-md-5 pt-lg-9 pb-3">{data.title}</h2>
 
-          <!-- Contact Information Row -->
-          <div class="row mb-5">
-        <div class="col-md-12" id="contact-data">
-          <!-- ADDRESS -->
-          <div class="card border-0 bg-transparent">
-            <div class="card-body">
-              <span class="fa fa-home" aria-hidden="true"></span>
-              &nbsp;
-              {meta?.street}, {meta?.postal}
-              {meta?.city}
+        <!-- Contact Information Row -->
+        <div class="row mb-5">
+          <div class="col-md-7" id="contact-image">
+            <div class="image-wrapper">
+              <img
+                src="/assets/img/geko_eingang.jpeg"
+                alt="Geko Eingang"
+                class="img-fluid rounded"
+              />
+            </div>
+          </div>
+          <div class="col-md-5" id="contact-data">
+            <!-- ADDRESS -->
+            <div class="card border-0 bg-transparent">
+              <div class="card-body">
+                <span class="fa fa-home" aria-hidden="true"></span>
+                &nbsp;
+                {meta?.street}, {meta?.postal} {meta?.city}
+              </div>
+            </div>
+
+            <div class="mb-3"></div>
+            <p>
+              <a href="#contact" class="fw-bold">Alle Telefonnummern und E-Mail-Adressen findet ihr hier</a>
+            </p>
+          </div>
+        </div>
+
+        <!-- Map and Content Row -->
+        <div class="row mb-5">
+          <!-- Left Column: Map -->
+          <div class="col-md-7">
+            <!-- Simple Map -->
+            <div id="map-image" class={showGoogleMap ? 'd-none' : ''}>
+              <div class="image-wrapper">
+                <img
+                  src="/assets/img/Geko_einfacheKarte_gross.png"
+                  alt="Einfache Karte"
+                  class="img-fluid rounded"
+                />
+              </div>
+            </div>
+
+            <!-- Google Map (Hidden by Default) -->
+            <div id="google-map" class={showGoogleMap ? '' : 'd-none'}>
+              <div class="ratio ratio-16x9">
+                <iframe
+                  src={`https://maps.google.com/maps?width=100%25&height=600&hl=de&q=${mapQuery}&t=&z=14&ie=UTF8&iwloc=B&output=embed`}
+                  allowfullscreen
+                  loading="lazy"
+                  title="Google Maps"
+                ></iframe>
+              </div>
+            </div>
+
+            <!-- Map Toggle Buttons -->
+            <div id="show-google-map" class="py-5" class:d-none={showGoogleMap}>
+              <button
+                type="button"
+                class="toggle-map-view btn-geko bg-geko-blue text-white px-3"
+                onclick={toggleMap}
+              >
+                <span class="text-white">Zeige Google-Karte</span>
+              </button>
+            </div>
+
+            <div id="show-simple-map" class="py-5" class:d-none={!showGoogleMap}>
+              <button
+                type="button"
+                class="toggle-map-view btn-geko bg-geko-blue text-white px-3"
+                onclick={toggleMap}
+              >
+                <span class="text-white">Zeige einfache Karte</span>
+              </button>
             </div>
           </div>
 
-          <div class="mb-3"></div>
-          <p>
-            <a href="#contact" class="fw-bold"
-              >Alle Telefonnummern und E-Mail-Adressen findet ihr hier</a
-            >
-          </p>
-        </div>
-      </div>
-
-      <!-- Map and Content Row -->
-      <div class="row mb-5">
-        <!-- Left Column: Map -->
-        <div class="col-md-7">
-          <!-- Simple Map -->
-          <div id="map-image" class={showGoogleMap ? 'd-none' : ''}>
-            {#if meta?.page_banner}
-              <img
-                src="/assets/img/Geko_einfacheKarte_gross.png"
-                alt="Einfache Karte"
-                class="img-fluid rounded w-100"
-              />
+          <!-- Right Column: Content -->
+          <div class="col-md-5">
+            {#if data?.content}
+              <div class="content-container">
+                {@html resolveRichText(data.content)}
+              </div>
             {/if}
           </div>
-
-          <!-- Google Map -->
-          <div id="google-map" class={showGoogleMap ? '' : 'd-none'}>
-            <div class="ratio ratio-16x9">
-              <iframe
-                src={`https://maps.google.com/maps?width=100%25&height=600&hl=de&q=${mapQuery}&t=&z=14&ie=UTF8&iwloc=B&output=embed`}
-                allowfullscreen
-                loading="lazy"
-                title="Google Maps"
-              ></iframe>
-            </div>
-          </div>
-
-          <!-- Map Toggle Buttons -->
-          <div id="show-google-map" class="py-5" class:d-none={showGoogleMap}>
-            <button
-              type="button"
-              class="toggle-map-view btn-geko bg-geko-blue text-white px-3"
-              onclick={toggleMap}
-            >
-              <span class="text-white">Zeige Google-Karte</span>
-            </button>
-          </div>
-
-          <div id="show-simple-map" class="py-5" class:d-none={!showGoogleMap}>
-            <button
-              type="button"
-              class="toggle-map-view btn-geko bg-geko-blue text-white px-3"
-              onclick={toggleMap}
-            >
-              <span class="text-white">Zeige einfache Karte</span>
-            </button>
-          </div>
-        </div>
-
-        <!-- Right Column: Content -->
-        <div class="col-md-5">
-          {#if data?.content}
-            <div class="content-container">
-              {@html resolveRichText(data.content)}
-            </div>
-          {/if}
-        </div>
-      </div>
         </div>
       </div>
     </div>
+  </div>
 </section>
+
+<style>
+  .image-wrapper {
+    width: 100%;
+    max-width: 100%;
+    overflow: hidden;
+    display: block;
+  }
+
+  .image-wrapper img {
+    max-width: 100% !important;
+    width: 100% !important;
+    height: auto !important;
+    display: block;
+    box-sizing: border-box;
+  }
+
+  #contact .col-md-7,
+  #contact .col-md-5 {
+    overflow: hidden;
+    box-sizing: border-box;
+  }
+
+  .content-container {
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    max-width: 100%;
+    overflow: hidden;
+  }
+
+  .content-container img {
+    max-width: 100% !important;
+    height: auto !important;
+    box-sizing: border-box;
+  }
+</style>

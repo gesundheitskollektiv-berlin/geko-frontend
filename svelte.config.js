a7d800e1c6dfa64a8f98044e1402d6ea92b353db
Geko Berlin <geko-berlin@systemli.org>
2026-01-23 09:23:55 +0100

    Improve svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [vitePreprocess()],
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -9,7 +11,8 @@ const config = {
     adapter: adapter(),
     prerender: {
       handleUnseenRoutes: 'warn',
-      handleMissingId: 'warn'
+      handleMissingId: 'warn',
+      handleHttpError: 'warn'
     }
   }
 };

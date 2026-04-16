8737da5d01239ddb4073ba5f0352abe726130dc3
Andreas Neumann <andneuma@posteo.de>
2026-04-16 19:25:03 +0200

    Restyle about block

--- src/lib/components/page-blocks/AboutBlock.svelte
+++ src/lib/components/page-blocks/AboutBlock.svelte
@@ -13,28 +13,30 @@
 <section id={sectionId} class={backgroundClass}>
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-lg-8 col-md-9 col-sm-11 my-5">
-          <h2>{data.title}</h2>
-
-          {@html resolveRichText(data.content)}
-
-          {#if data?.team_image}
-            <StrapiImage
-              asset={data.team_image}
-              alt={data.team_image.alternativeText || 'Team'}
-              class="img-fluid my-4 geko-image-rounded"
-            />
-          {/if}
-
-          <!-- Link to About Us subpage -->
-          <div class="mt-5">
-            <a class="text-decoration-underline text-geko-black fs-20 fw-bold" href="/{locale}/ueber_uns">
-              Mehr über uns
-            </a>
+      <div class="col-lg-9 col-md-10 col-sm-11 my-5">
+        <div class="row">
+          <div class="col-md-5">
+            <h2>{data.title}</h2>
+            {@html resolveRichText(data.content)}
+            <div class="mt-4">
+              <a class="text-decoration-underline text-geko-black fs-20 fw-bold" href="/{locale}/ueber_uns">
+                Mehr über uns
+              </a>
+            </div>
           </div>
+          {#if data?.team_image}
+            <div class="col-md-7 d-flex align-items-center">
+              <StrapiImage
+                asset={data.team_image}
+                alt={data.team_image.alternativeText || 'Team'}
+                class="img-fluid geko-image-rounded"
+              />
+            </div>
+          {/if}
         </div>
       </div>
     </div>
+  </div>
 </section>
 
 <style>
